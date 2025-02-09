b34c1ca94b88d536ecd9181eb97ccd73f2837fbc
xsmh <80352285+xsmh@users.noreply.github.com>
2025-02-09 18:38:13 +0300

    Fix font

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static int smartgaps          = 0;        /* 1 means no outer gap when there is 
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = {
-  "RobotoMono Nerd Font:pixelsize=13:antialias=true:autohint=true",
+  "JetBrainsMono Nerd Font:style=SemiBold:pixelsize=13:antialias=true:autohint=true",
   "monospace:size=10", 
 };
 static char normbgcolor[]           = "#222222";
