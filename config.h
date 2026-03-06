112117e357543a5c04fa31b69406c48258c4207f
xsmh <sh@smh.sh>
2026-03-06 23:39:08 +0300

    Change browser to firefox

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* Constants */
 #define TERMINAL "st"
 #define TERMCLASS "St"
-#define BROWSER "google-chrome-stable"
+#define BROWSER "firefox"
 
 /* appearance */
 static unsigned int borderpx  = 3;        /* border pixel of windows */
